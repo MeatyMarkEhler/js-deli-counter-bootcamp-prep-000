27eeff564d77bb64b49538e4e297b8723595ce06
Mark Ehler <ehlerm42@gmail.com>
2018-08-18 15:13:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 function takeANumber(line, name){
   if (line === 0) {
     line.unshift(name);
-    return `Welcome, ${name}. You are number ${line.length} in line.`
+    return `Welcome, ${name}. You are number ${line.length} in line.`;
   } else {
-    line.push(name)
-    return `Welcome, ${name}. You are number ${line.length} in line.`
+    line.push(name);
+    return `Welcome, ${name}. You are number ${line.length} in line.`;
   }
 }
 
-//`Welcome, Ada. You are number ${line + 1} in line.`
 
-//function nowServing(line) {if line.unshift(); return "now serving: ${name**index position 0**}" ; else; return "There is nobody waiting to be served!"}
+function nowServing(line) {
+  if line === 0 {
+    return "There is nobody waiting to be served!";
+  } else { line.shift();
+    return `now serving: ${line[0]}`
+  }
+}
 
 //announces the first item and .shift() the remaining line
 

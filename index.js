6e7ba9f1b8937edcce5011bfa6301dc2745dd9c6
Mark Ehler <ehlerm42@gmail.com>
2018-08-18 16:19:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function nowServing(line) {
 function currentLine(line) {
   let linelist = []
   if (line.length === 0); {
-  return `The line is currently empty`; 
+  return `The line is currently empty.`; 
   } for (i = 0; i < line.length; i++ ); {
     linelist.push( `${i + 1}, ${line} `);
   }

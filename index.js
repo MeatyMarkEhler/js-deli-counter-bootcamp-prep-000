10c7b6b33c5c1f6ead80c96608708f0446a0b631
Mark Ehler <ehlerm42@gmail.com>
2018-08-18 16:45:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ function nowServing(line) {
 
 function currentLine(line) {
   const linelist = []
-  if (line.length === 0); {
-    return `The line is currently empty.`; 
-  } for (i = 0; line.length > i; i++); {
+  for (i = 0; line.length > i; i++); {
     linelist.push(`${i + 1}. ${line[i]}`);
+  } else (line.length === 0); {
+    return `The line is currently empty.`;
   }
   return `The like is currently: ${linelist.join(', ')}`
 }

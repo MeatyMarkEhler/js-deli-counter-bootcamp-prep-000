b30c6a438a8371f922c7df52e38f85ed685a98cc
Mark Ehler <ehlerm42@gmail.com>
2018-08-18 13:52:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function takeANumber(line, name){
+  line.push(name)
+  if (line>0)
+  line.unshift(name)
+  
+}
+
+//`Welcome, Ada. You are number ${line + 1} in line.`
+
+//function nowServing(line) {if line.unshift(); return "now serving: ${name**index position 0**}" ; else; return "There is nobody waiting to be served!"}
+
+//announces the first item and .shift() the remaining line
+
+//function currentLine(line) {return line.each[+1]; `The line is currently: ${line[+1]} else; "The line is currently: empty."

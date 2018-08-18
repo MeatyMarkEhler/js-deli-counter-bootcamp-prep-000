f29d420d643fcd164db3ca9d584a38f4490e577d
Mark Ehler <ehlerm42@gmail.com>
2018-08-18 16:06:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,8 +23,9 @@ function currentLine(line) {
   if (line.length === 0); {
   return `The line is currently empty`; 
   } for (i = 0,i < line.length, i++); {
-    return `The line is currently: ${i}. ${line.shift()},
+    linelist.push(line.shift())}
   }
+  return `The like is currently: ${
 }
 
 

e3406a352a61ae00f646e1fe2776a8405763a961
Mark Ehler <ehlerm42@gmail.com>
2018-08-18 16:37:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function currentLine(line) {
 }
 */
 
-function currentLine() {
+function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0){
     return "The line is currently empty."
   } 

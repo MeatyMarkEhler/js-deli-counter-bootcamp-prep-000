7410658b2ac9dd690b0ff448317563dc06ba3d0f
Mark Ehler <ehlerm42@gmail.com>
2018-08-18 16:36:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function nowServing(line) {
   }
 }
 
-
+/*
 function currentLine(line) {
   const linelist = []
   if (line.length === 0); {
@@ -26,4 +26,20 @@ function currentLine(line) {
     linelist.push(`${i + 1}`, `${line[i]}`);
   }
   return `The like is currently: ${linelist.join(', ')}`
+}
+*/
+
+function currentLine() {
+  if (katzDeliLine.length === 0){
+    return "The line is currently empty."
+  } 
+  const nosAndNoms = [];
+  
+  for (i = 0; katzDeliLine.length > i; i++){
+  
+  nosAndNoms.push(`${i+1}. ${katzDeliLine[i]}`);
+  }
+   return `The line is currently: ${nosAndNoms.join(", ")}` 
+  
+  
 }
